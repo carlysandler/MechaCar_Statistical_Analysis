117417f82d553a1d57f20de4badb786db29ae624
Carly Sandler <carly.sandler@gmail.com>
2021-05-28 13:52:59 -0400

    adding D2: Summary Statistics technical analysis

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,20 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #Determine the p-value and the r-squared value for linear regression model using `summary()` function
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mecha_mpg))
+
+#Deliverable 2: Summary Statistics on Suspension Coils
+
+#Import and read in the Suspension_Coil.csv file as a table
+mecha_coil <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+#Create a total_summary dataframe to get summary statistics of PSI column
+total_summary <- mecha_coil %>% summarize(Mean_PSI=mean(PSI),
+                                          Median_PSI=median(PSI),
+                                          Var_PSI=var(PSI),
+                                          SD_PSI =sd(PSI),.groups = 'keep')
+
+#Create a lot_summary dataframe. Group eacb manufacturing lot by the mean, median, variance and std.dev of PSI column
+lot_summary <- mecha_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
+                                                                        Median_PSI=median(PSI),
+                                                                        Var_PSI=var(PSI),
+                                                                        SD_PSI =sd(PSI),.groups = 'keep')
